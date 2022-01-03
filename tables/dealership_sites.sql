538d30739bd92b08eb62fb012772c6aa687feb6d
martin-trk <68852515+martin-trk@users.noreply.github.com>
2022-01-03 15:06:47 +0100

    Update dealership_sites.sql

--- tables/dealership_sites.sql
+++ tables/dealership_sites.sql
@@ -1,6 +1,6 @@
 CREATE TABLE dealership_sites (
 	site_id	NUMBER,
-	NAME VARCHAR2(20) NOT NULL,
+	name VARCHAR2(20) NOT NULL,
 	mod_user VARCHAR2(300),
 	created_on TIMESTAMP(6),
 	last_mod TIMESTAMP(6),
